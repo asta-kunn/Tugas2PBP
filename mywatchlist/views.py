96b169ede2f0b3d880a9c4b33c578b0b69f6943b
rifqibradley12 <rifqibradley12@gmail.com>
2022-09-22 00:45:28 +0700

    update

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -36,19 +36,23 @@ def show_json(request):
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def show_html(request):
-    data = MyWatchlistItem.objects.all()
+    item_mywatchlist = MyWatchlistItem.objects.all()
+    belum_tonton = 0
+    watched = 0 
+    pesan = ""
+    for film in item_mywatchlist:
+        if (film.watched):
+            watched += 1
+        else:
+            belum_tonton += 1
+    if (watched >= belum_tonton):
+        pesan = "Selamat, kamu sudah banyak menonton!"
+    else:
+        pesan = "Wah, kamu masih sedikit menonton!"
     context = {
-        'item_mywatchlist': data,
+        'item_mywatchlist': item_mywatchlist,
         'nama' : 'Rifqi',
+        "pesan" : pesan,
     }
     return render(request, 'mywatchlist.html', context)
-
-    
-def show_json_by_id(request, id):
-    data = MyWatchlistItem.objects.filter(pk=id)
-    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
-
-def show_xml_by_id(request, id):
-    data = MyWatchlistItem.objects.filter(pk=id)
-    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
