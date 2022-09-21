ed52a7079af680652c1365c19b30f1ce5032374e
rifqibradley12 <rifqibradley12@gmail.com>
2022-09-22 00:49:03 +0700

    fix

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from mywatchlist.views import show_mywatchlist
 from mywatchlist.views import show_xml
 from mywatchlist.views import show_json
-from mywatchlist.views import show_json_by_id
-from mywatchlist.views import show_xml_by_id
 from mywatchlist.views import show_html
 
 app_name = 'mywatchlist'
@@ -12,8 +10,6 @@ urlpatterns = [
     path('', show_mywatchlist, name='show_mywatchlist'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
-    path('json/<int:id>', show_json_by_id, name='show_json_by_id'),
-    path('xml/<int:id>', show_xml_by_id, name='show_xml_by_id'),
     path('html/', show_html, name='show_html'),
    
 ]
