c50b91415c0782dd9bc047b36279400e6df9169c
rifqibradley12 <rifqibradley12@gmail.com>
2022-09-21 12:55:22 +0700

    update

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -22,14 +22,6 @@ def show_json(request):
     data = MyWatchlistItem.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
-def show_json_by_id(request, id):
-    data = MyWatchlistItem.objects.filter(pk=id)
-    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
-
-def show_xml_by_id(request, id):
-    data = MyWatchlistItem.objects.filter(pk=id)
-    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
-
 def show_html(request):
     data = MyWatchlistItem.objects.all()
     context = {
@@ -39,3 +31,11 @@ def show_html(request):
     return render(request, 'mywatchlist.html', context)
 
     
+def show_json_by_id(request, id):
+    data = MyWatchlistItem.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = MyWatchlistItem.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
