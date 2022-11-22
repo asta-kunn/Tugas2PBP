fa132f073be5809d50ce37307b6ad2fb129050e5
rifqibradley12 <rifqibradley12@gmail.com>
2022-11-23 02:55:04 +0700

    update

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -5,5 +5,5 @@ class MyWatchlistItem(models.Model):
     watched = models.BooleanField(default=False)
     title = models.CharField(max_length=255)
     rating = models.FloatField()
-    release_date = models.TextField()
+    release_date = models.DateField()
     review = models.TextField()
