38ce63eb8d68ef4d1c9272382bdbb497c26e1b60
rifqibradley12 <rifqibradley12@gmail.com>
2022-11-22 16:02:22 +0700

    models watchlist

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class MyWatchlistItem(models.Model):
-    watched = models.TextField()
+    watched = models.booleanField(default=False)
     title = models.CharField(max_length=255)
     rating = models.FloatField()
     release_date = models.TextField()
